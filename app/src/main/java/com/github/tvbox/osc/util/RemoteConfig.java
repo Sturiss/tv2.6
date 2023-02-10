2db1fc70baa2b11cd707dd34bc853c04b35c1278
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2023-02-10 16:36:16 +0800

    Update RemoteConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
@@ -27,15 +27,10 @@ public class RemoteConfig {
 
     public static void Init(Context mContext){
         RemoteConfig.mContext = mContext;
-        /*if (ToolUtils.isApkInDebug(mContext)){
-            remoteUrl = "https://0dlj.cn/config";
-        }else{
-            remoteUrl = "https://0dlj.cn/config";
-        }*/
         if (ToolUtils.isApkInDebug(mContext)){
-            remoteUrl = "https://gitee.com/wei1120/dwei/raw/master/remote.ini";
+            remoteUrl = "https://0dlj.cn/config";
         }else{
-            remoteUrl = "https://gitee.com/wei1120/dwei/raw/master/remote.ini";
+            remoteUrl = "https://0dlj.cn/config";
         }
         LOG.e("RemoteConfig",
                 ToolUtils.isApkInDebug(mContext) ? "当前处于【调试】模式":"当前处于【正式】模式",
